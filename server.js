a63a6b6a4ae7d50b33986e763ce6e3b016c04be0
Jérôme <jeromemeyer23@gmail.com>
2019-07-13 20:22:26 +0200

    Angular-cli auto-deploy

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.use(morgan('dev'));
 app.use('/', serveStatic(path.join(__dirname, 'app')));
 
 const port = 5000;
-app.listen(port);
+app.listen(process.env.PORT || 8888)
 
 console.log('Server listening on port', port);
